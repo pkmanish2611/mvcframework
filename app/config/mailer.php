41cb53376ba787f60f9dfe158fe42ed5a6c0dd3e
pkmanish2611 <pkmanish2611@gmail.com>
2021-05-23 05:24:26 +0530

    mailer secure

--- app/config/mailer.php
+++ app/config/mailer.php
@@ -12,9 +12,8 @@ $mail->SMTPAuth = true; //Enable SMTP authentication
 $mail->Username = 'bookshelf2611@gmail.com'; //SMTP username
 $mail->Password = 'Mani@12345'; //SMTP password
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-$mail->Port = 587; //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+$mail->Port = 587; //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above 
 
- 
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 $mail->isHTML(true); //Set email format to HTML
